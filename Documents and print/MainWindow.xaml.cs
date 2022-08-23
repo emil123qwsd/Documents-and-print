115ab0b2198b2c8b412a6469541459ef0de17ce0
эмиль ахметханов <emil.ah@yandex.ru>
2022-08-23 22:41:13 +0300

    Добавьте файлы проекта.

---
+++ Documents and print/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Xps.Packaging;
+using System.IO;
+using System.Windows.Xps;
+using System.Xaml;
+
+namespace Documents_and_print
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            docViewer.ClearValue(FlowDocumentScrollViewer.DocumentProperty);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            using (FileStream fs=File.Open("1.xaml", FileMode.Create)) 
+            {
+                XamlWriter.Save(docViewer.Document, fs);
+            }
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            using (FileStream fs = File.Open("1.xaml", FileMode.Open))
+            {
+                docViewer.Document = XamlReader.Load (fs) as FlowDocument;
+            }
+        }
+    }
+}
